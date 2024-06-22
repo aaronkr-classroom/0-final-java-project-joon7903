41cb21b3405bc8b85cd85444d23c5722234deb09
joon7903 <162276719+joon7903@users.noreply.github.com>
2024-06-22 21:02:58 +0900

    lol
    lol

--- src/BookMarket.java
+++ src/BookMarket.java
@@ -1,10 +1,7 @@
 
 public class BookMarket {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Shopping Mall!");
-		System.out.println("Welcome to BookMarket");
+		System.out.println("Welcome to Shopping Mall");
+		System.out.println("Welcome to Book Market!");
 	}
-
 }
